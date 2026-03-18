deb4b6f6d13387b918380020e902cebbdb23dee1
Gedipudi Darshani <139340574+Gedipudidarshani@users.noreply.github.com>
2026-03-18 12:36:03 +0530

    Create EndoVisionApplication.java

---
+++ backend/EndoVisionApplication.java
@@ -0,0 +1,11 @@
+package com.endovision;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EndoVisionApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EndoVisionApplication.class, args);
+    }
+}
